832f01c2c5ddbfd47e12924ab770c4dc29cde227
donymyounglee <pajang1515@daum.net>
2023-08-22 14:39:19 +0900

    댓글 쓰기 댓글보기 완료

--- src/components/pages/Layout/StoryBoard.js
+++ src/components/pages/Layout/StoryBoard.js
@@ -10,6 +10,9 @@ const StoryBoard = () => {
   const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태를 관리
   const [content, setContent] = useState(""); // 게시글 내용 상태
   const [attachment, setAttachment] = useState(null); // 첨부 파일 상태
+  const [isCommentModalOpen, setIsCommentModalOpen] = useState(false);
+  const [showReplyInput, setShowReplyInput] = useState(false); // State for reply input visibility
+
 
 
   useEffect(() => {
@@ -55,6 +58,14 @@ const StoryBoard = () => {
     setIsModalOpen(false);
   };
 
+  const openCommentModal = () => {
+    setIsCommentModalOpen(true);
+  };
+
+  const closeCommentModal = () => {
+    setIsCommentModalOpen(false);
+  };
+
   const handleContentChange = (e) => {
     setContent(e.target.value);
   };
@@ -70,7 +81,9 @@ const StoryBoard = () => {
     closeModal();
   };
 
-
+  const openReplyInput = () => {
+    setShowReplyInput(!showReplyInput);
+  };
 
   return (
     <div className={classes.storyBoardWrap}>
@@ -117,7 +130,7 @@ const StoryBoard = () => {
               <span>1,245</span>
               <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/my-cdn-d39b2.appspot.com/o/bookmark.png?alt=media&token=bd0b81ce-e724-47a7-9556-75a48130dc3d" />
             </div>
-            <div className={classes.comentFlex}>
+            <div className={classes.comentFlex} onClick={openCommentModal}>
               <span>2,345</span>
               <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/my-cdn-d39b2.appspot.com/o/coment%20(1).png?alt=media&token=525ce0da-4368-4d4c-82d1-e170845a6cc9" />
             </div>
@@ -171,7 +184,8 @@ const StoryBoard = () => {
             </div>
           </div>
         </div>
-      </div><div className={classes.createBoard2}>
+      </div>
+      <div className={classes.createBoard2}>
         <div className={classes.titleWrap}>
           <h2 className={classes.suggestion}>추천 게시물</h2>
         </div>
@@ -237,6 +251,74 @@ const StoryBoard = () => {
             />
             <button onClick={handleSubmit}>작성</button>
             <button onClick={closeModal}>닫기</button>
+          </div>
+        </div>
+      )}
+
+      {/*댓글*/}
+      {isCommentModalOpen && (
+        <div className={classes.commentModalOverlay}>
+          <div className={classes.commentModalContent}>
+            <h3 className={classes.commentModalTitle}>Comments</h3>
+            <div className={classes.commentsContainer}>
+              {/* Comment */}
+              <div className={classes.commentWrap}>
+                <div className={classes.comment}>
+                  <div className={classes.commentHeader}>
+                    <p className={classes.commentAuthor}>김정수</p>
+                    <button onClick={openReplyInput} className={classes.replyButton}>답글쓰기</button>
+                  </div>
+                  <div style={{width : '100%', display : 'flex', justifyContent : 'space-between', alignItems : 'center'}}>
+                    <p className={classes.commentText}>스쿼드 진짜 구리다</p>
+                    <div className={classes.commentActions}>
+                      {/* Like button */}
+                      <button style={{display : 'flex', alignItems : 'center'}}  className={classes.likeButton}>
+                        <img
+                          src="https://firebasestorage.googleapis.com/v0/b/my-cdn-d39b2.appspot.com/o/like.svg?alt=media&token=57793a04-e2df-4f9c-abe5-2445f83e4a57"
+                          alt="Like"
+                        />
+                        <span>123</span>
+                      </button>
+                    </div>
+                  </div>
+
+                </div>
+                {/* Reply */}
+                <div className={classes.reply}>
+                  <div className={classes.commentHeader}>
+                    <p className={classes.commentAuthor}>채오성</p>
+                  </div>
+                  <p className={classes.commentText}>너보단 나을 듯?</p>
+                </div>
+                <div className={classes.reply}>
+                  <div className={classes.commentHeader}>
+                    <p className={classes.commentAuthor}>박성무</p>
+                  </div>
+                  <p className={classes.commentText}>그러게요</p>
+                </div>
+                {/* Reply Input */}
+                {showReplyInput && (
+                  <div className={classes.replyInput}>
+                    <input
+                      type="text"
+                      placeholder="당신의 의견을 적어 주세요."
+                      className={classes.replyInputField}
+                    />
+                    <button className={classes.replySubmitButton}>등록</button>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className={classes.commentForm}>
+        <textarea
+          className={classes.commentTextarea}
+          placeholder="Add a comment"
+        ></textarea>
+              <button className={classes.commentSubmitButton}>댓글쓰기</button>
+            </div>
+            <button className={classes.closeButton} onClick={closeCommentModal}>
+              Close
+            </button>
           </div>
         </div>
       )}
